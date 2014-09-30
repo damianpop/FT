c59cfdf8496bf1fb0c5d008c9e688277946f9afc
Damian <damian@Damians-Mac-Pro.local>
2014-09-29 23:32:31 -0300

    The view moves up on small iPhone screen when editing
    To give space for all the fields and the search button.

--- FT/SearchViewController.m
+++ FT/SearchViewController.m
@@ -43,6 +43,63 @@
     return NO;
 }
 
+-(void)keyboardWillShow {
+    if (self.view.frame.origin.y >= 0) {
+        [self setViewMovedUp:YES];
+    }
+    else if (self.view.frame.origin.y < 0) {
+        [self setViewMovedUp:NO];
+    }
+}
+
+-(void)keyboardWillHide {
+    if (self.view.frame.origin.y >= 0) {
+        [self setViewMovedUp:YES];
+    }
+    else if (self.view.frame.origin.y < 0) {
+        [self setViewMovedUp:NO];
+    }
+}
+
+-(void)setViewMovedUp:(BOOL)movedUp {
+    int offset = 512 - [[UIScreen mainScreen] bounds].size.height;
+    if(offset > 0){
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:0.3];
+    
+        CGRect rect = self.view.frame;
+        if (movedUp) {
+            rect.origin.y -= offset;
+            rect.size.height += offset;
+            self.tableView.tableHeaderView.hidden = YES;
+        }  else  {
+            rect.origin.y += offset;
+            rect.size.height -= offset;
+            self.tableView.tableHeaderView.hidden = NO;
+        }
+        self.view.frame = rect;
+    
+        [UIView commitAnimations];
+    }
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    // register for keyboard notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow) name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated  {
+    [super viewWillDisappear:animated];
+    // unregister for keyboard notifications while not visible.
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
 - (void)loadOffersUserID:(NSString*) userId apiKey:(NSString*)apiKey locale:(NSString*)locale ip:(NSString*)ip {
     [self.searchButton setEnabled:NO];
     [self.activityIndicator startAnimating];
